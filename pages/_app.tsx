d0c10b498805350bed2034583f3b59a8a490b2df
payhelpa <dev@payhelpa.com>
2023-08-07 20:57:05 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/app1c4a.css'
+import  '../styles/bundle1c4a.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
