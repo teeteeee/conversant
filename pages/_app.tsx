29448d6af10489c3a24e843d52a748ee80661902
payhelpa <dev@payhelpa.com>
2023-08-11 05:05:10 +0100

    google login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,19 @@ import '../styles/globals.css'
 import '../styles/app1c4a.css'
 import  '../styles/bundle1c4a.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp
+
+// pages/_app.tsx
